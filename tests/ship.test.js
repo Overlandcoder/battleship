dd2362382e5076c5df67d0554afa76056ea251be
Overlander <anuragnandal@hotmail.com>
2025-12-17 18:07:23 -0500

    test(Ship): add isSunk test and use describe blocks to group tests

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,30 +1,44 @@
 import Ship from "../src/ship.js";
 
-test("creates a ship with the specified length", () => {
-  const testShip = new Ship(4);
-  expect(testShip.length).toBe(4);
+describe("initial Ship state", () => {
+  test("creates a ship with the specified length", () => {
+    const testShip = new Ship(4);
+    expect(testShip.length).toBe(4);
+  });
+
+  test("ship has 0 hits upon creation", () => {
+    const testShip = new Ship(4);
+    expect(testShip.hits).toBe(0);
+  });
+
+  test("ship is not sunk upon creation", () => {
+    const testShip = new Ship(4);
+    expect(testShip.isSunk).toBe(false);
+  });
 });
 
-test("ship has 0 hits upon creation", () => {
-  const testShip = new Ship(4);
-  expect(testShip.hits).toBe(0);
+describe("hit() method", () => {
+  test("hit() method increases hits by 1", () => {
+    const testShip = new Ship(4);
+    expect(testShip.hits).toBe(0);
+    testShip.hit();
+    expect(testShip.hits).toBe(1);
+  });
+
+  test("ship can receive multiple hits", () => {
+    const testShip = new Ship(4);
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.hits).toBe(2);
+  });
 });
 
-test("ship is not sunk upon creation", () => {
-  const testShip = new Ship(4);
-  expect(testShip.isSunk).toBe(false);
-});
-
-test("hit() method increases hits by 1", () => {
-  const testShip = new Ship(4);
-  expect(testShip.hits).toBe(0);
-  testShip.hit();
-  expect(testShip.hits).toBe(1);
-});
-
-test("ship can receive multiple hits", () => {
-  const testShip = new Ship(4);
-  testShip.hit();
-  testShip.hit();
-  expect(testShip.hits).toBe(2);
+describe("isSunk property logic", () => {
+  test("ship is sunk after hits equals length", () => {
+    const testShip = new Ship(2);
+    testShip.hit();
+    expect(testShip.isSunk).toBe(false);
+    testShip.hit();
+    expect(testShip.isSunk).toBe(true);
+  });
 });
