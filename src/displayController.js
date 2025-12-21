940942f53ae54f1037e5ba4b1ae135bfcf43a753
Overlander <anuragnandal@hotmail.com>
2025-12-21 13:08:32 -0500

    feat: implement ship placement and render ships

--- src/displayController.js
+++ src/displayController.js
@@ -6,7 +6,8 @@ function createDisplayController() {
       const rowDiv = document.createElement("div");
       for (let col = 0; col <= 9; col++) {
         const btn = document.createElement("button");
-        btn.textContent = `${row},${col}`;
+        const ship = board.squareAt(col, row);
+        btn.textContent = ship ? "[]" : "-";
         rowDiv.appendChild(btn);
       }
       container.appendChild(rowDiv);
--- src/gameController.js
+++ src/gameController.js
@@ -6,6 +6,16 @@ function createGameController() {
   const humanPlayer = new Player("player", new Gameboard());
   const computerPlayer = new Player("computer", new Gameboard());
   const display = createDisplayController();
+  placeShips(humanPlayer);
+  placeShips(computerPlayer);
+
+  function placeShips(player) {
+    player.board.placeShip(5, 0, 0, "horizontal");
+    player.board.placeShip(4, 2, 2, "horizontal");
+    player.board.placeShip(3, 4, 4, "vertical");
+    player.board.placeShip(3, 6, 6, "horizontal");
+    player.board.placeShip(2, 6, 8, "vertical");
+  }
 
   function displayBoards() {
     display.displayBoard(humanPlayer.board);
