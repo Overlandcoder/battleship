5f6599f3b0901bf2cb18a8e6eb73686d91958aa7
Overlander <anuragnandal@hotmail.com>
2025-12-21 14:51:16 -0500

    feat(display): apply basic styling to board and squares

--- src/displayController.js
+++ src/displayController.js
@@ -2,19 +2,24 @@ function createDisplayController() {
   const container = document.querySelector(".container");
 
   function displayBoard(board) {
+    const boardDiv = document.createElement("div");
+    boardDiv.classList.add("board");
+
     for (let row = 0; row <= 9; row++) {
       const rowDiv = document.createElement("div");
+      rowDiv.classList.add("row");
       for (let col = 0; col <= 9; col++) {
         const btn = createButton(board.squareAt(col, row));
         rowDiv.appendChild(btn);
       }
-      container.appendChild(rowDiv);
+      boardDiv.appendChild(rowDiv);
     }
+    container.appendChild(boardDiv);
   }
 
   function createButton(square) {
     const btn = document.createElement("button");
-    btn.textContent = square ? "[]" : "-";
+    btn.classList.add(square ? "ship" : "water");
     return btn;
   }
 
